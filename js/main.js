676de73cf26994c3e1f03e032aef1ec5b3d13cd6
Pawel Gorczyca <pawelgorczyca@gmail.com>
2017-05-08 15:29:04 +0200

    JavaScript quick review

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var variable1 = "Number 1";
+var variable2 = "Numer 2";
+var variable3 = 3;
+var variable4 = 2;
+
+console.log(variable3+variable4*1246);
+
+var variable5 = document.getElementById("formularz").poleTekstowe.value;
+console.log(variable2.length);
+
+
+var array1 = [1,2,3];
+var array2 = array1;
+array2.push(4);
+console.log(array1);
+console.log(array2);
+
+
+var variableX = "Zmienna typu znakowego ";
+var variableY = 5;
+var variableZ = "4";
+
+console.log(variableX + variableY);
+console.log(variableZ - variableY);
+console.log(variableY - variableZ);
+console.log(variableX - variableY);
+console.log(variableX - variableZ);
+console.log(typeof(variable1, variable2, variable3, variable4, variable5, variableX, variableY, variableZ));
